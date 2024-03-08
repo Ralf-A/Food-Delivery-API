ee453f7224a679d4b745d738e18d26f7c8af9bda
Ralf-A <ralfanimagi@gmail.com>
2024-03-08 14:54:08 +0200

    Initial commit

---
+++ Food-Delivery-API/src/test/java/com/FujitsuFoodDeliveryAPI/Food/Delivery/API/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FujitsuFoodDeliveryAPI.Food.Delivery.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
